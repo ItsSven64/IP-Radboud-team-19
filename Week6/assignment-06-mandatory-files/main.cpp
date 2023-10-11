be672e7460f58eae6047ca6509d4303ed7cc9605
Sven de Wit <svendewit1707@gmail.com>
2023-10-11 20:02:29 +0200

    Part1

--- Week6/assignment-06-mandatory-files/main.cpp
+++ Week6/assignment-06-mandatory-files/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <cassert>
-
+ 
 using namespace std;
 
 enum Cell {Dead=0, Live};                         // a cell is either Dead or Live (we use the fact that dead = 0 and live = 1)
@@ -11,17 +11,56 @@ const char LIVE             = '*' ;               // the presentation of a live 
 const int NO_OF_ROWS        = 40 ;                // the number of rows (height) of the universe (both on file and screen)
 const int NO_OF_COLUMNS     = 60 ;                // the number of columns (width) of the universe (both on file and screen)
 
+ifstream input_file;
+//Opens input file, and returns true if suceeded
+
+bool open_file(ifstream& in_file, string filename){
+    assert(true);
+    in_file.open(filename);
+    return !in_file.fail();
+}
+
+Cell char_to_cell(char in){
+    if (in == LIVE) return Live;
+    else return Dead;
+}
+
+char cell_to_char(Cell in){
+    if (in == Dead) return DEAD;
+    else return LIVE;
+}
+
 // Part 1: get cell in bounded universe
 Cell cell_at (Cell universe [NO_OF_ROWS][NO_OF_COLUMNS], int row, int column)
 {
     // pre-conditions, post-conditions, implementation
-    return Dead;
+    //Make sure if the requested value is in-bounds
+    assert((row>0)&&(column>0));
+    if (row>NO_OF_COLUMNS||column>NO_OF_COLUMNS) return Dead;
+    else return universe[NO_OF_COLUMNS][NO_OF_COLUMNS];
 }
 
 // Part 2: setting the scene
 bool read_universe_file (string filename, Cell universe [NO_OF_ROWS][NO_OF_COLUMNS])
 {
-    // pre-conditions, post-conditions, implementation
+    char in;
+    Cell out;
+    int row=0;
+    //is file open?
+    assert(open_file(input_file, filename));
+    for (int i=0; i<=40; i++){
+         in = input_file.get();
+         //if new-line, go to the next row, else just save the next character
+         if (in=='\n') {
+            row++;
+            in = input_file.get();
+         }
+
+         universe [row][i] = in;
+
+
+
+    }
     return false;
 }
 
@@ -39,7 +78,9 @@ void next_generation (Cell now [NO_OF_ROWS][NO_OF_COLUMNS], Cell next [NO_OF_ROW
 #ifndef TESTING
 int main ()
 {
-    // pre-conditions, post-conditions, implementation
+    string filename = "10_cell_row.txt";
+    open_file(input_file, filename);
+    cell_at()
     return 0;
 }
 #endif
